628c8457c5e686f6f721c447ae37e81bea1deac4
wessaqqq <sadovskayayul@gmail.com>
2026-07-02 17:30:29 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 window.BYBANK_CONFIG = {
   url: "https://jmejirwilnhjtqjxnfvn.supabase.co",
   key: "sb_publishable_IBQjJZehbLTkQb7kEpRL1w_yzobE_Kw",
-  // Пароль на вход в приложение. Смени на свой и скажи Маше. Пустая строка = без пароля.
-  password: "ddma2026"
+  // Пароль на вход в приложение. Пустая строка = без пароля.
+  password: ""
 };
